410fc438d41780ccb271a0180f34e0034d6dfc26
D4nielS4les <dsr.daniel.sales@gmail.com>
2023-11-08 16:02:57 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 // Pegar os elementos da DOM
 const nome = document.getElementById("nome");
 const idade = document.getElementById("idade");
+const img = document.getElementById("heroi");
 const select = document.getElementById("tipo");
-const tipo = select.value;
+const tipo = select.options[select.selectedIndex].value;
 const h1 = document.getElementById("h1");
 const btn = document.getElementById("btn");
-console.log(tipo)
+
 class Heroi {
-    constructor(nome, idade, tipo) {
-        this.nome = nome;
-        this.idade = idade;
-        this.tipo = tipo;
-    }
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+    this.img = img;
+  }
 
-    atacar() {
-        let ataque;
-        switch (this.tipo) {
-            case "Mago":
-                ataque = this.magia();
-                break;
-            case "Guerreiro":
-                ataque = this.espada();
-                break;
-            case "Monge":
-                ataque = this.artes();
-                break;
-            case "Ninja":
-                ataque = this.shuriken();
-                break
-            default:
-                ataque = "Nada"
-        }
-        h1.innerHTML = `O ${this.tipo} atacou usando ${ataque}`;
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case "mago":
+        ataque = this.magia();
+        break;
+      case "guerreiro":
+        ataque = this.espada();
+        break;
+      case "monge":
+        ataque = this.artes();
+        break;
+      case "ninja":
+        ataque = this.shuriken();
+        break;
+      default:
+        ataque = "nenhum especial";
     }
+    h1.textContent = `O ${this.tipo} atacou usando ${ataque}`;
+    img.src = `img/${this.tipo}.png`;
+  }
 
-    magia() {
-        return "Magia";
-    }
+  magia() {
+    return "magia";
+  }
 
-    espada() {
-        return "Espada";
-    }
+  espada() {
+    return "espada";
+  }
 
-    shuriken() {
-        return "Shuriken";
-    }
+  shuriken() {
+    return "shuriken";
+  }
 
-    artes() {
-        return "Artes Marciais";
-    }
+  artes() {
+    return "artes marciais";
+  }
 }
 
 btn.addEventListener("click", function () {
-    const tipo = select.value; // Pegue o valor do select no clique do botão
-    const heroi = new Heroi(nome.value, idade.value, tipo);
-    heroi.atacar();
-})
+  // Pegar o valor do select no clique do botão
+  let tipo = select.value;
+  // Criar um novo heroi com os valores dos inputs
+  let heroi = new Heroi(nome.value, idade.value, tipo);
+  // Chamar o método atacar do heroi
+  heroi.atacar();
+});
